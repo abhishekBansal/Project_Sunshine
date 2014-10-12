6a540f4f5ee5353ed26f05945de5faaa01019c99
Abhishek Bansal <discover.ab@gmail.com>
2014-10-12 19:19:26 +0530

    + adding database backend + relevant test cases + completing chapter 4a

---
+++ app/src/androidTest/java/com/udacity/abhishekbansal/sunshine/test/FullTestSuite.java
@@ -0,0 +1,21 @@
+package com.udacity.abhishekbansal.sunshine.test;
+
+/**
+ * Created by Abhishek Bansal on 10/12/2014.
+ */
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
